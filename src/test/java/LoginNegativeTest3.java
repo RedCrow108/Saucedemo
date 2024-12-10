0100e9c412b0c7a5ada866497703377da053e257
VBrokert <brokertva@gmail.com>
2024-12-10 19:59:35 +0700

    3-й негативный тест

--- src/test/java/LoginNegativeTest3.java
+++ src/test/java/LoginNegativeTest3.java
@@ -3,7 +3,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
-
 import java.time.Duration;
 
 public class LoginNegativeTest3 {
