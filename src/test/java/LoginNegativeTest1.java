1c59609dfbb997c93227055e274484007b122ced
VBrokert <brokertva@gmail.com>
2024-12-10 19:58:59 +0700

    1-й негативный тест

--- src/test/java/LoginNegativeTest1.java
+++ src/test/java/LoginNegativeTest1.java
@@ -22,27 +22,9 @@ public class LoginNegativeTest1 {
         driver.findElement(By.name("password")).sendKeys("aafaeefefaf"); // Ввод не валидного пароля
         driver.findElement(By.cssSelector("#login-button")).click(); // Нажатие на кнопку логина
         String errorMessage = driver.findElement(By.cssSelector("#login_button_container > div > form > div.error-message-container.error > h3")).getText();
-        Assert.assertEquals(errorMessage, "Epic sadface: Username and password do not match any user in this service");
+        Assert.assertEquals(errorMessage, "Epic sadface: Username and password do not match any user in this service"); // Проверка, что получено соответствующее сообщение об ошибке
     }
 
-    @Test
-    public void LoginNeg2() {
-        Assert.assertTrue(driver.findElement(By.cssSelector("#login-button")).isDisplayed()); // Проверка, что находимся на странице с кнопкой логина
-        driver.findElement(By.name("user-name")).sendKeys("qwqfwqf"); // Ввод не валидного логина
-        driver.findElement(By.name("password")).sendKeys("secret_sauce"); // Ввод пароля
-        driver.findElement(By.cssSelector("#login-button")).click(); // Нажатие на кнопку логина
-        String errorMessage = driver.findElement(By.cssSelector("#login_button_container > div > form > div.error-message-container.error > h3")).getText();
-        Assert.assertEquals(errorMessage, "Epic sadface: Username and password do not match any user in this service");
-    }
-
-    @Test
-    public void LoginNeg3() {
-        Assert.assertTrue(driver.findElement(By.cssSelector("#login-button")).isDisplayed()); // Проверка, что находимся на странице с кнопкой логина
-        driver.findElement(By.name("password")).sendKeys("secret_sauce"); // Ввод не валидного пароля
-        driver.findElement(By.cssSelector("#login-button")).click(); // Нажатие на кнопку логина
-        String errorMessage = driver.findElement(By.cssSelector("#login_button_container > div > form > div.error-message-container.error > h3")).getText();
-        Assert.assertEquals(errorMessage, "Epic sadface: Username is required");
-    }
 
     @AfterClass(alwaysRun = true)
     public void tearDown() {
