227a1eb2ac613fe621fa13e3727a000700ba1b07
VBrokert <brokertva@gmail.com>
2025-02-15 00:40:32 +0700

    added headless

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,7 +2,9 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 import pages.CartPage;
@@ -24,10 +26,14 @@ public class BaseTest {
     @BeforeMethod
     public void setup(@Optional("chrome") String browser) {
         if (browser.equalsIgnoreCase("chrome")) {
-            driver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            driver = new ChromeDriver(options);
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         } else if (browser.equalsIgnoreCase("edge")) {
-            driver = new EdgeDriver();
+            EdgeOptions optionsE =new EdgeOptions();
+            optionsE.addArguments("--headless");
+            driver = new EdgeDriver(optionsE);
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         }
         loginPage = new LoginPage(driver);
